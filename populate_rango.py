90f0bf4b6f0c78b6cd50829ac70d74f3740a1ad2
Anas Yahya <2446628y@student.gla.ac.uk>
2019-01-29 19:34:13 +0200

    Ch6 take 1

--- populate_rango.py
+++ populate_rango.py
@@ -8,25 +8,26 @@ def populate():
 
 	python_pages = [
 		{"title": "Official Python Tutorial",
-		"url":"http://docs.python.org/2/tutorial/"},
+		"url":"http://docs.python.org/2/tutorial/"
+		 ,"views":15},
 
 		{"title":"How to Think like a Computer Scientist",
-		"url":"http://www.greenteapress.com/thinkpython/"},
+		"url":"http://www.greenteapress.com/thinkpython/","views":32},
 		{"title":"Learn Python in 10 Minutes",
 
-		"url":"http://www.korokithakis.net/tutorials/python/"} ]
+		"url":"http://www.korokithakis.net/tutorials/python/","views":18} ]
 	django_pages = [
 		{"title":"Official Django Tutorial",
-		"url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+		"url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/","views":37},
 		{"title":"Django Rocks",
-		"url":"http://www.djangorocks.com/"},
+		"url":"http://www.djangorocks.com/","views":26},
 		{"title":"How to Tango with Django",
-		"url":"http://www.tangowithdjango.com/"} ]
+		"url":"http://www.tangowithdjango.com/","views":145} ]
 	other_pages = [
 		{"title":"Bottle",
-		"url":"http://bottlepy.org/docs/dev/"},
+		"url":"http://bottlepy.org/docs/dev/","views":10},
 		{"title":"Flask",
-		"url":"http://flask.pocoo.org"} ]
+		"url":"http://flask.pocoo.org","views":55} ]
 
 	cats = {"Python": {"pages": python_pages,"likes":64,"views":128},
 		"Django": {"pages": django_pages,"likes":32,"views":64},
@@ -34,14 +35,14 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat,cat_data["likes"],cat_data["views"])
 		for p in cat_data["pages"]:
-			add_page(c, p["title"], p["url"])
+			add_page(c, p["title"], p["url"],p["views"])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
 			print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
 	p = Page.objects.get_or_create(category=cat, title=title)[0]
 	p.url=url
 	p.views=views
